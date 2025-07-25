db9b976aeff08c336f5887ddee5b757807dfb82f
LunarDev <101055131+waysset0@users.noreply.github.com>
2025-07-25 16:20:27 +0300

    Add files via upload

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,7 @@
+import os
+
 class Config:
-    TELEGRAM_TOKEN = "6508364781:AAEroStjy3iwfbY75z3fOjEp9Sni8__7Cvs"
-    VK_TOKEN = "e19ffa99e19ffa99e19ffa9995e2a8c57dee19fe19ffa99891f35011d0bafcb1c2cc40d"
+    TELEGRAM_TOKEN = os.environ['BOT_TOKEN']
+    VK_TOKEN = os.environ['VK_TOKEN']
     CHECK_INTERVAL = 60
-    STORAGE_FILE = "subscriptions.json"
+    STORAGE_FILE = "/data/subscriptions.json"
